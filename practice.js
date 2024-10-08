e5c4c8cc5426fab74e96a1a48dc96633893c71ce
Derrick Furtado <derrick.furtado10@gmail.com>
2024-10-07 18:30:31 -0600

    EOD

--- practice.js
+++ practice.js
@@ -2,9 +2,15 @@ console.log("Hello, world")
 
 console.log("Did this run?")
 
-let x = 5
+let x = 5.0000
 let y = 24
+
+x = 12.40
+y = 12.592
 
 z = x + y
 
 console.log("X plus Y = ", z)
+
+console.log(typeof(x))
+
---
+++ practice.ts
@@ -0,0 +1,13 @@
+// console.log("Hello, world")
+
+// console.log("Did this run?")
+
+// var x = 5.0000
+// var y = 24
+
+
+// let z = x + y
+
+// console.log("X plus Y = ", z)
+
+// console.log(typeof(x))
