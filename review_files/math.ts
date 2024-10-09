3d8a1bb89673ce7eed59c7952152eb413ed5d8f8
Derrick Furtado <derrick.furtado10@gmail.com>
2024-10-09 13:51:31 -0600

    Added Typescript compiler to run TS files on HTML

---
+++ review_files/math.ts
@@ -0,0 +1,13 @@
+let pi = Math.PI
+
+// console.log(pi)
+
+let randomNum = Math.floor(Math.random() * 10) + 1
+console.log(`A random number between 0 and 10 is: `, randomNum)
+
+
+console.log(typeof randomNum)
+
+
+// console.log(`Comparison: `, "21" == 21)     // True
+// console.log(`Comparison: `, "21" == 2)      // False
---
+++ src/runningCode.ts
@@ -0,0 +1,6 @@
+console.log(`runningCode.ts connected`)
+
+
+alert("Hello, World!")
+
+
