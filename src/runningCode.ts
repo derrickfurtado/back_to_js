6cfbad84afc167071caf7a776e57914feb030aa9
Derrick Furtado <derrick.furtado10@gmail.com>
2024-10-11 12:00:37 -0600

    lunch

--- src/runningCode.ts
+++ src/runningCode.ts
@@ -3,6 +3,57 @@ console.log(`runningCode.ts connected`)
 
 // alert("Hello, World!")
 
-console.error("Uh Oh!")
+// console.error("Uh Oh!")
 
 
+let day = 1
+
+// if (day === 1) {                 /// not very easy to read
+//     console.log("Monday")
+// }
+// else if (day === 2) {
+//     console.log(`Tuesday`)
+// }
+// else if (day === 3) {
+//     console.log(`Wednesday`)
+// }
+// else if (day === 4) {
+//     console.log(`Thursday`)
+// }
+// else if (day === 5) {
+//     console.log(`Friday`)
+// }
+// else if (day === 6) {
+//     console.log(`Saturday`)
+// }
+// else if (day === 7) {
+//     console.log(`Sunday`)
+// }
+
+day = 4
+
+switch(day) {                       /// easier to read
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thrusday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("Saturday")
+        break
+    case 7:
+        console.log("Sunday")
+        break
+    default:
+        console.log("Invalid day")
+}
