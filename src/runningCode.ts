8530b39538d9339e12ea4acdfd8220ec771a5ad4
Derrick Furtado <derrick.furtado10@gmail.com>
2024-10-28 14:10:26 -0600

    more TS work

--- src/runningCode.ts
+++ src/runningCode.ts
@@ -1,6 +1,27 @@
 console.log(`runningCode.ts connected`)
 
 
+const button = document.getElementById("button")! as HTMLButtonElement;
+const input1 = document.getElementById("name")! as HTMLInputElement;
+const input2 = document.getElementById("age")! as HTMLInputElement;
+const input3 = document.getElementById("city")! as HTMLInputElement;
+const input4 = document.getElementById("state")! as HTMLInputElement;
+
+
+
+function addName (name : string, age : string, city : string, state : string) {
+    return name + age + city + state
+}
+
+button.addEventListener("click", function() {
+    console.log(addName(input1.value, input2.value, input3.value, input4.value));
+})
+
+
+///////////////////////////
+
+
+
 // alert("Hello, World!")
 
 // console.error("Uh Oh!")
@@ -75,18 +96,64 @@ day = 5
 // console.log(color)
 
 
-function fibonachi(n:number) {
-    let fibo = [0, 1]
+// function fibonachi(n:number) {
+//     let fibo = [0, 1]
     
-    if(n === 0) return [0]
-    if(n === 1) return [0,1]
+//     if(n === 0) return [0]
+//     if(n === 1) return [0,1]
         
-    for(let i = 2; i <= n; i++){
-        let next = fibo[i-1] + fibo[i-2]
-        fibo.push(next)
+//     for(let i = 2; i <= n; i++){
+//         let next = fibo[i-1] + fibo[i-2]
+//         fibo.push(next)
+//     }
+//     fibo.splice(1,1)
+//     return fibo
+// }
+
+// console.log(fibonachi(10))
+
+function add(n1:number, n2: number, showResult : boolean) {
+    if (showResult) {
+        console.log("If True: ", n1 + n2)
+    } else {
+        return n1 + n2
     }
-    fibo.splice(1,1)
-    return fibo
 }
 
-console.log(fibonachi(10))
+
+let ifFalse = add(2,4.5, false)
+
+console.log("if False: ", ifFalse)
+
+
+///////////////////////////////////////////////////////////
+
+
+const family = {
+    father: {
+        name: "Derrick",
+        age: 40,
+        location: "Lehi",
+        state: "Utah"
+    },
+    mother: {
+        name: "Sedina",
+        age: 37,
+        location: "Lehi",
+        state: "Utah"
+    },
+    son: {
+        name: "Daylan",
+        age: 7,
+        location: "Lehi",
+        state: "Utah"
+    },
+    daughter: {
+        name: "Hana",
+        age: 5,
+        location: "Lehi",
+        state: "Utah"
+    }
+};
+
+console.log(family.mother.name)
